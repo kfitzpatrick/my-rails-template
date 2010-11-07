cff905583135cf378b4a640b05a5eba336904d4f
Kevin Fitzpatrick <kfitzpatrick@Kevin-Fitzpatricks-MacBook-Pro.local>
2010-11-06 18:19:36 -0700

    Use bundler

--- awesome_app.rb
+++ awesome_app.rb
@@ -11,6 +11,31 @@ config/database.yml
 db/*.sqlite3
 END
 
+log run "gem install bundler -v=1.0.2"
+
+file 'Gemfile', <<-END
+  source :gemcutter
+  gem "rails", "2.3.8"
+  gem "sqlite3-ruby", :require => "sqlite3"
+
+  # bundler requires these gems in all environments
+  # gem "nokogiri", "1.4.2"
+  # gem "geokit"
+
+  group :development do
+    # bundler requires these gems in development
+    # gem "rails-footnotes"
+  end
+
+  group :test do
+    # bundler requires these gems while running tests
+    # gem "rspec"
+    # gem "faker"
+  end
+END
+
+log run "bundle install"
+
 rake "db:migrate"
 rake "default"
 
