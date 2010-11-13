327e19d62355278a93e3d6fcccdf3e9c622e18f9
Kevin Fitzpatrick <kfitzpatrick@Kevin-Fitzpatricks-MacBook-Pro.local>
2010-11-12 20:15:04 -0800

    Added remarkable

--- awesome_app.rb
+++ awesome_app.rb
@@ -29,87 +29,104 @@ unless (run "gem list bundler").include?("1.0.2")
 end
 
 file 'Gemfile', <<-END
-  source :gemcutter
-  gem "rails", "2.3.8"
-  gem "sqlite3-ruby", :require => "sqlite3"
+source :gemcutter
+gem "rails", "2.3.8"
+gem "sqlite3-ruby", :require => "sqlite3"
 
-  group :development do
-  end
+group :development do
+end
 
-  group :test do
-    gem "rspec", "> 1.3", "< 2.0"
-    gem "rspec-rails", "> 1.3", "< 2.0"
-    gem "remarkable_rails", "3.1.13"
-    gem "capybara", "0.4.0"
-    gem "cucumber-rails", "0.3.2"
-    gem "database_cleaner", "0.5.2"
-  end
+group :test do
+  gem "rspec", "> 1.3", "< 2.0"
+  gem "rspec-rails", "> 1.3", "< 2.0"
+  gem "remarkable_rails", "3.1.13"
+  gem "capybara", "0.4.0"
+  gem "cucumber-rails", "0.3.2"
+  gem "database_cleaner", "0.5.2"
+end
 END
 
-log run "bundle"
+# log run "bundle"
 
 generate "cucumber"
 generate "rspec"
 generate "nifty_layout"
 generate "nifty_scaffold Foo name:string"
 
+file 'spec/spec_helper.rb', <<-END_OF_SPEC_HELPER
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
+require 'spec/autorun'
+require 'spec/rails'
+require 'remarkable_rails'
+
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
+Spec::Runner.configure do |config|
+  config.use_transactional_fixtures = true
+  config.use_instantiated_fixtures  = false
+  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+end
+END_OF_SPEC_HELPER
+
+
 # Rewrite the controller spec to use remarkable
 run "rm spec/controllers/foos_controller_spec.rb"
 file 'spec/controllers/foos_controller_spec.rb', <<-EOF
   require File.dirname(__FILE__) + '/../spec_helper'
 
-  describe FoosController do
-    mock_models :foo
+describe FoosController do
+  mock_models :foo
 
-    describe :get => :index do
-      should_render_template :index
-    end
-
-    describe :get => :show, :id => "foo" do 
-      expects :find, :on => Foo, :with => "foo", :returns => mock_foo 
-      should_render_template :show
-    end
-
-    describe :get => :new do
-      should_render_template :new
-    end
-
-    describe :post => :create do
-      expects :new, :on => Foo, :returns => mock_foo 
-      expects :save, :on => mock_foo, :returns => false  
-      should_render_template :new
-    end
-
-    describe :post => :create, :foo => "bar" do
-      expects :new, :on => Foo, :with => "bar", :returns => mock_foo 
-      expects :save, :on => mock_foo, :returns => true  
-      should_redirect_to { foo_path(mock_foo) }
-      should_assign_to :foo, :with => mock_foo
-    end
-
-     describe :get => :edit, :id => "foo"  do
-       expects :find, :on => Foo, :returns => mock_foo
-       should_render_template :edit
-     end
-
-    describe :put => :update, :id => "foo" do
-      expects :find, :on => Foo, :returns => mock_foo
-      expects :update_attributes, :on => mock_foo, :returns => false  
-      should_render_template :edit 
-    end
-
-     describe :put => :update, :id => "foo", :foo => "bar" do
-       expects :find, :on => Foo, :returns => mock_foo
-       expects :update_attributes, :on => mock_foo, :with => "bar", :returns => true
-       should_redirect_to { foo_path(mock_foo) } 
-     end
-
-     describe :delete => :destroy, :id => "foo" do
-       expects :find, :on => Foo, :returns => mock_foo
-       expects :destroy, :on => mock_foo
-       should_redirect_to { foos_path }
-     end
+  describe :get => :index do
+    should_render_template :index
   end
+
+  describe :get => :show, :id => "foo" do 
+    expects :find, :on => Foo, :with => "foo", :returns => mock_foo 
+    should_render_template :show
+  end
+
+  describe :get => :new do
+    should_render_template :new
+  end
+
+  describe :post => :create do
+    expects :new, :on => Foo, :returns => mock_foo 
+    expects :save, :on => mock_foo, :returns => false  
+    should_render_template :new
+  end
+
+  describe :post => :create, :foo => "bar" do
+    expects :new, :on => Foo, :with => "bar", :returns => mock_foo 
+    expects :save, :on => mock_foo, :returns => true  
+    should_redirect_to { foo_path(mock_foo) }
+    should_assign_to :foo, :with => mock_foo
+  end
+
+   describe :get => :edit, :id => "foo"  do
+     expects :find, :on => Foo, :returns => mock_foo
+     should_render_template :edit
+   end
+
+  describe :put => :update, :id => "foo" do
+    expects :find, :on => Foo, :returns => mock_foo
+    expects :update_attributes, :on => mock_foo, :returns => false  
+    should_render_template :edit 
+  end
+
+   describe :put => :update, :id => "foo", :foo => "bar" do
+     expects :find, :on => Foo, :returns => mock_foo
+     expects :update_attributes, :on => mock_foo, :with => "bar", :returns => true
+     should_redirect_to { foo_path(mock_foo) } 
+   end
+
+   describe :delete => :destroy, :id => "foo" do
+     expects :find, :on => Foo, :returns => mock_foo
+     expects :destroy, :on => mock_foo
+     should_redirect_to { foos_path }
+   end
+end
 EOF
 
 run "rm spec/fixtures/foos.yml"
@@ -127,13 +144,13 @@ git :add => "."
 git :commit => "-a -m 'Initial commit'"
 
 # TODO
-# insert the require remarkable bits into spec_helper
-# install remarkable and make the controller use it
 # one basic cucumber feature
 # one javascript cucumber feature
 # Install jQuery
 # make an application.js with the basic stuff I want
 # install jasmine 
+# one jasmine test
+# a javascript object with the inheritance style that you like
 # nifty layout
 # one scaffolded resource with tests
 # devise stuff
